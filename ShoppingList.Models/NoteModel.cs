183f7cef0429b4af1f8add926572334560e73a04
Jennifer <jrlawyer@gmail.com>
2016-09-07 16:03:25 -0400

    I re-created the NoteModel and removed the extra folders.

---
+++ ShoppingList.Models/NoteModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.Models
+{
+    public class NoteModel
+    {
+        public int NoteId { get; set; }
+
+        public int ShoppingListItemId { get; set; }
+
+        [Required]
+        [MaxLength(8000)]
+        public string Body { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifieDateTimeOffset { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{NoteId}]";
+        }
+    }
+}
