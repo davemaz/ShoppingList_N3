4c3de5357fab272fae7d1f0584207d0b7d6d9e0b
Jennifer <jrlawyer@gmail.com>
2016-09-07 13:51:43 -0400

    I added the ShoppingListCreateModel and added some Required decorator attributes to the ShoppingListItemModel and the ShoppingListModel.

--- ShoppingList.Models/ShoppingListCreateModel.cs
+++ ShoppingList.Models/ShoppingListCreateModel.cs
@@ -1,8 +1,17 @@
-﻿namespace ShoppingList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.Models
 {
     public class ShoppingListCreateModel
     {
+        [Required]
+        public string Name { get; set; }
 
+        public string Color { get; set; }
 
+        public override string ToString()
+        {
+            return $"[New] {Name}";
+        }
     }
 }
