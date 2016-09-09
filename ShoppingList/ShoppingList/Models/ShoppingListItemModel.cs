1204ae3f7b15edec1494f41baf72ceeaaec6a3de
Jennifer <jrlawyer@gmail.com>
2016-09-09 09:50:50 -0400

    Starting over with new project; models are included.

---
+++ ShoppingList/ShoppingList/Models/ShoppingListItemModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.Models
+{
+    public class ShoppingListItemModel
+    {
+        public int ShoppingListItemId { get; set; }
+
+        public int ShoppingListId { get; set; }
+
+        [Required]
+        [MaxLength(8000)]
+        public string Content { get; set; }
+
+        //created separate file for enum.
+        public enum Priority
+        {
+            [Display(Name = "It Can Wait.")]
+            ItCanWait = 0,
+            [Display(Name = "Need It Soon")]
+            NeedItSoon = 1,
+            [Display(Name = "Get It Now!")]
+            GetItNow = 2
+        }
+
+
+        [MaxLength(8000)]
+        public string Note { get; set; }
+
+        public bool IsChecked { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{ShoppingListId}]";
+        }
+
+        public virtual ShoppingListModel ShoppingListModel { get; set; }
+
+    }
+}
