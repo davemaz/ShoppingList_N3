740f38d33d8400386d2f4e3a52c61dd1eb543ab5
Jennifer <jrlawyer@gmail.com>
2016-09-09 16:04:59 -0400

    Looking at controllers.

--- ShoppingList/ShoppingList/Controllers/ShoppingListModelController.cs
+++ ShoppingList/ShoppingList/Controllers/ShoppingListModelController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using ShoppingList.Models;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using ShoppingList.Models;
 
 namespace ShoppingList.Controllers
 {
