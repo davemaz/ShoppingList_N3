5d3be9d519aa01bcf4346157ac8391eb03652e4e
chriswainscott <chrisdwianscott@gmail.com>
2016-09-14 08:51:13 -0400

    added documentation

--- ShoppingList/ShoppingList/Controllers/ShoppingListItemController.cs
+++ ShoppingList/ShoppingList/Controllers/ShoppingListItemController.cs
@@ -17,6 +17,22 @@ namespace ShoppingList.Controllers
         // GET: ShoppingListItem
         public ActionResult Index()
         {
+
+            var list = new List<ShoppingListItem>
+            {
+                 new ShoppingListItem{ShoppingListItemModelId = 1, Note = "Aquafina", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 2, Note = "Mulshi Springs", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 3, Note = "Alfa Blue", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 4, Note = "Atlas Premium", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 5, Note = "Bailley", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 6, Note = "Bisleri", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 7, Note = "Himalayan", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 8, Note = "Cool Valley", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 9, Note = "Dew Drops", IsChecked = false},
+                 new ShoppingListItem{ShoppingListItemModelId = 10, Note = "Dislaren", IsChecked = false},
+
+            };
+
             return View(db.ShoppingListItems.ToList());
         }
 
